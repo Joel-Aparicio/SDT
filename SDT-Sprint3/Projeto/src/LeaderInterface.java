8230788ff2b2a3607d60c3181ed1d8110c4c3042
Csoares00 <cfsoares.99@gmail.com>
2024-11-28 20:41:47 +0000

    Delete SDT-Sprint3 directory

--- SDT-Sprint3/Projeto/src/LeaderInterface.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-import java.util.List;
-
-public interface LeaderInterface extends Remote {
-    void sendAck(String uuid) throws RemoteException;
-    void uploadFile(File file) throws RemoteException;
-    void sendSetup(String uuid) throws  RemoteException;
-}
